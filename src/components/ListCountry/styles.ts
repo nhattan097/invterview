04468128664b2e4df963da1ae6533ee3ebb44ff1
nhattan097 <nhattan097@yahoo.com>
2021-08-21 17:02:49 +0700

    style skeleton

---
+++ src/components/ListCountry/styles.ts
@@ -0,0 +1,9 @@
+import { makeStyles, Theme } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme: Theme) => ({
+  skeleton: {
+    borderRadius: 8,
+  },
+}));
+
+export default useStyles;
