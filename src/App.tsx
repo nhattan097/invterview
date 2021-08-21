7b5fb60968b0093c2eb05570ef80b00a9b7d3849
nhattan097 <nhattan097@yahoo.com>
2021-08-21 17:03:35 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Container, CssBaseline, Box } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+import HomePage from './pages/home';
+import theme from './themes';
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <ReactQueryDevtools />
+        <Box component="main" paddingBottom={4}>
+          <Container>
+            <HomePage />
+          </Container>
+        </Box>
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
