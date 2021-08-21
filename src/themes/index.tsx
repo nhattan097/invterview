2e45ec38336c2dc89ba512b1b6fed8fe98b55f82
nhattan097 <nhattan097@yahoo.com>
2021-08-21 16:57:09 +0700

    add theme

---
+++ src/themes/index.tsx
@@ -0,0 +1,11 @@
+import { createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: '#F2F3FA',
+    },
+  },
+});
+
+export default theme;
